c6d4b19ad7b5af4feb72611909a31d6cdce56ce5
Victim Musundire <mvpvictim@gmail.com>
2023-09-22 01:06:06 +0200

    springboot and mysql

---
+++ src/main/java/com/victim/springboot/services/UserService.java
@@ -0,0 +1,17 @@
+package com.victim.springboot.services;
+
+import com.victim.springboot.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    User createUser(User user);
+
+    User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
+
+    void deleteUser(Long userId);
+}
